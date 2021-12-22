59f3fb0eaaf6f1507d69e1e2bc712307df30b71d
Jaan Toots <jaan@jaantoots.org>
2021-12-23 00:49:45 +0200

    Improve Day 22 Part 2

--- day22-p2/main.go
+++ day22-p2/main.go
@@ -66,33 +66,30 @@ func (a *Cuboid) Intersect(b *Cuboid) *Cuboid {
 	return nil
 }
 
-func (c *Cuboid) split(ax int, values []int) []*Cuboid {
-	cs := []*Cuboid{}
-	for _, x := range values {
-		left, right := c.Copy(), c.Copy()
-		left.axes[ax].max = x - 1
-		right.axes[ax].min = x
-		cs = append(cs, left)
-		c = right
-	}
-	cs = append(cs, c)
-	return cs
+func (c *Cuboid) split(ax, x int) (*Cuboid, *Cuboid) {
+	left, right := c.Copy(), c.Copy()
+	left.axes[ax].max = x - 1
+	right.axes[ax].min = x
+	return left, right
 }
 
 func (a *Cuboid) fragment(sub *Cuboid) []*Cuboid {
-	stack := []*Cuboid{a}
+	all := []*Cuboid{}
+	next := a
+	var left, middle, right *Cuboid
 	for i, seg := range sub.axes {
-		next := []*Cuboid{}
-		for _, c := range stack {
-			for _, s := range c.split(i, []int{seg.min, seg.max + 1}) {
-				if s.valid() {
-					next = append(next, s)
-				}
-			}
+		left, middle = next.split(i, seg.min)
+		middle, right = middle.split(i, seg.max+1)
+		if left.valid() {
+			all = append(all, left)
 		}
-		stack = next
+		if right.valid() {
+			all = append(all, right)
+		}
+		next = middle
 	}
-	return stack
+	all = append(all, next)
+	return all
 }
 
 func (a *Cuboid) Combine(b *Cuboid) ([]*Cuboid, []*Cuboid) {
@@ -123,6 +120,46 @@ func (a *Cuboid) Combine(b *Cuboid) ([]*Cuboid, []*Cuboid) {
 	return frags, extra
 }
 
+func addCuboid(cuboids *list.List, cuboid *Cuboid) {
+	stack := list.New()
+	stack.PushBack(cuboid)
+	var a, b *Cuboid
+	for e := stack.Front(); e != nil; e = e.Next() {
+		b = e.Value.(*Cuboid)
+		var match bool
+		for d := cuboids.Front(); d != nil; d = d.Next() {
+			a = d.Value.(*Cuboid)
+			frags, extra := a.Combine(b)
+			for _, f := range frags {
+				cuboids.PushBack(f)
+			}
+			for _, f := range extra {
+				stack.PushBack(f)
+			}
+			if frags != nil {
+				cuboids.Remove(d)
+				match = true
+				break
+			}
+		}
+		if !match && b.state {
+			cuboids.PushBack(b)
+		}
+	}
+}
+
+func onCount(cuboids *list.List) int {
+	var count int
+	var c *Cuboid
+	for e := cuboids.Front(); e != nil; e = e.Next() {
+		c = e.Value.(*Cuboid)
+		if c.state {
+			count += c.Volume()
+		}
+	}
+	return count
+}
+
 func main() {
 	var state string
 	var x0, x1, y0, y1, z0, z1 int
@@ -137,49 +174,8 @@ func main() {
 		}
 		cuboid := Cuboid{state == "on", [3]Segment{Segment{x0, x1}, Segment{y0, y1}, Segment{z0, z1}}}
 		fmt.Println(cuboid)
-
-		stack := list.New()
-		stack.PushBack(&cuboid)
-		var a, b *Cuboid
-		for e := stack.Front(); e != nil; e = e.Next() {
-			b = e.Value.(*Cuboid)
-			var match bool
-			for d := cuboids.Front(); d != nil; d = d.Next() {
-				a = d.Value.(*Cuboid)
-				frags, extra := a.Combine(b)
-				for _, f := range frags {
-					cuboids.PushBack(f)
-				}
-				for _, f := range extra {
-					stack.PushBack(f)
-				}
-				if frags != nil {
-					cuboids.Remove(d)
-					match = true
-					break
-				}
-			}
-			if !match && b.state {
-				cuboids.PushBack(b)
-			}
-		}
-		var count int
-		var c *Cuboid
-		for e := cuboids.Front(); e != nil; e = e.Next() {
-			c = e.Value.(*Cuboid)
-			if c.state {
-				count += c.Volume()
-			}
-		}
-		fmt.Println(count)
+		addCuboid(cuboids, &cuboid)
+		//fmt.Println(onCount(cuboids))
 	}
-	var count int
-	var c *Cuboid
-	for e := cuboids.Front(); e != nil; e = e.Next() {
-		c = e.Value.(*Cuboid)
-		if c.state {
-			count += c.Volume()
-		}
-	}
-	fmt.Println(count)
+	fmt.Println(onCount(cuboids))
 }
